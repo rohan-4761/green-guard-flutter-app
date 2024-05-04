d56da08db1c25596eb92fc37fdb930812a30af26
Reubemx <110336575+Reubemx@users.noreply.github.com>
2024-05-04 23:35:00 +0530

    first commit

--- lib/pages/ImageDisplay.dart
+++ lib/pages/ImageDisplay.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'chatScreen.dart';
 import 'login_page.dart';
 
-class ImageDisplayPage extends StatelessWidget {
+class ImageDisplayPage extends StatefulWidget {
   final String imagePath;
+  final String responseBody;
 
-  const ImageDisplayPage({super.key, required this.imagePath});
+  const ImageDisplayPage({
+    super.key,
+    required this.imagePath,
+    required this.responseBody,
+  });
 
+  @override
+  _ImageDisplayPageState createState() => _ImageDisplayPageState();
+}
+
+class _ImageDisplayPageState extends State<ImageDisplayPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,16 +40,19 @@ class ImageDisplayPage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(5),
                 ),
                 child: Image.file(
-                  File(imagePath),
+                  File(widget.imagePath),
                   width: 300,
                   height: 300,
                   fit: BoxFit.cover,
                 ),
               ),
               const SizedBox(height: 25),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+              Text(
+                '${widget.responseBody}',
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
               const SizedBox(height: 16),
@@ -59,9 +72,9 @@ class ImageDisplayPage extends StatelessWidget {
                       MaterialPageRoute(builder: (context) => const LoginForm()),
                     );
                   }
-
                 },
-                child: const Text('Talk to Chatbot',
+                child: const Text(
+                  'Talk to Chatbot',
                   style: TextStyle(
                     color: Colors.black,
                   ),
@@ -74,3 +87,107 @@ class ImageDisplayPage extends StatelessWidget {
     );
   }
 }
+
+
+
+
+// import 'dart:io';
+// import 'dart:typed_data';
+//
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'chatScreen.dart';
+// import 'login_page.dart';
+//
+// class ImageDisplayPage extends StatelessWidget {
+//   final String imagePath;
+//   final String responseBody;
+//
+//
+//   const ImageDisplayPage({
+//     Key? key,
+//     required this.imagePath,
+//     required this.responseBody,
+//   }) : super(key: key);
+//
+//   //
+//   // const ImageDisplayPage({
+//   //   Key? key,
+//   //   required this.imagePath, required Float32List prediction,
+//   // }) : super(key: key);
+//
+//   // const ImageDisplayPage({super.key, required this.imagePath, required Float32List prediction});
+//
+//
+//
+//   // get prediction => null;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Selected Image'),
+//       ),
+//       body: Center(
+//         child: SingleChildScrollView(
+//           padding: const EdgeInsets.all(16),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Container(
+//                 padding: const EdgeInsets.all(16),
+//                 decoration: BoxDecoration(
+//                   border: Border.all(color: Colors.grey),
+//                   borderRadius: BorderRadius.circular(5),
+//                 ),
+//                 child: Image.file(
+//                   File(imagePath),
+//                   width: 300,
+//                   height: 300,
+//                   fit: BoxFit.cover,
+//                 ),
+//               ),
+//               const SizedBox(height: 25),
+//                TextField(
+//                 readOnly: true,
+//                 decoration: InputDecoration(
+//                   border: OutlineInputBorder(),
+//                   labelText: 'Prediction',
+//                 ),
+//                 controller: TextEditingController(text: widget.responseBody),
+//
+//
+//               ),
+//               const SizedBox(height: 16),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   User? user = FirebaseAuth.instance.currentUser;
+//                   if (user != null) {
+//                     // User is logged in, navigate to ChatScreen
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => const ChatScreen()),
+//                     );
+//                   } else {
+//                     // If not logged in, navigate to login page
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => const LoginForm()),
+//                     );
+//                   }
+//
+//                 },
+//                 child: const Text('Talk to Chatbot',
+//                   style: TextStyle(
+//                     color: Colors.black,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'ImageDisplay.dart';
 import 'chatScreen.dart';
 import 'login_page.dart';
+import 'package:http/http.dart' as http;
+
 
 class Cam extends StatefulWidget {
   const Cam({Key? key});
@@ -28,20 +31,57 @@ class _CamState extends State<Cam> {
     _handleImage(pickedImage);
   }
 
-  void _handleImage(XFile? pickedImage) {
+  Future<String> sendImage(XFile? pickedImage) async {
+    if (pickedImage == null) {
+      print('No image selected');
+      return 'None';
+    }
+
+    String url = 'https://b709-34-73-189-37.ngrok-free.app/prediction';
+
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+
+    var image = await http.MultipartFile.fromPath(
+      'image',
+      pickedImage.path,
+    );
+
+    request.files.add(image);
+
+    try {
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        print('Response body: ${response.body}');
+        return response.body;
+      }
+      else {
+        print('Request failed with status code: ${response.statusCode}');
+        return '';
+      }
+    }
+    catch (e) {
+      print('Error sending request: $e');
+      return '';
+    }
+
+
+  }
+
+  void _handleImage(XFile? pickedImage) async{
+    String responseBody = await sendImage(pickedImage);
+
     if (pickedImage != null) {
-      // Navigate to a new page to display the image
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => ImageDisplayPage(imagePath: pickedImage.path),
+          builder: (context) =>
+              ImageDisplayPage(imagePath: pickedImage.path, responseBody: responseBody),
         ),
       );
-    } else {
-      print('No image selected');
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -143,85 +183,3 @@ class _CamState extends State<Cam> {
     );
   }
 }
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData(
-//         primaryColor: Colors.grey[500], // Grey color scheme
-//       ),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Green Guard'),
-//         ),
-//         backgroundColor: Colors.grey[700],
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               ElevatedButton(
-//                 onPressed: () {
-//                   _openCamera();
-//                 },
-//                 style: ElevatedButton.styleFrom(
-//                   padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-//                 ),
-//                 child: const Text(
-//                   'Camera',
-//                   textAlign: TextAlign.center,
-//                   style: TextStyle(
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//               ),
-//               const SizedBox(height: 16),
-//               ElevatedButton(
-//                 onPressed: _openGallery,
-//                 style: ElevatedButton.styleFrom(
-//                   padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-//                 ),
-//                 child: const Text(
-//                   'Upload From Gallery',
-//                   textAlign: TextAlign.center,
-//                   style: TextStyle(
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//               ),
-//               const SizedBox(height: 16),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   User? user = FirebaseAuth.instance.currentUser;
-//                   if (user != null) {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(builder: (context) => const ChatScreen()),
-//                     );
-//                   } else {
-//                     // If not logged in, navigate to login page
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(builder: (context) => const LoginForm()),
-//                     );
-//                   }
-//                 },
-//                 style: ElevatedButton.styleFrom(
-//                   padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-//                 ),
-//                 child: const Text(
-//                   'Chat with Bot',
-//                   textAlign: TextAlign.center,
-//                   style: TextStyle(
-//                     fontSize: 20,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
